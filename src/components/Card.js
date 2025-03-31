9dfd3db1a535d7261da3f554786f940c3c1fa8c1
ArmenGharibyan <gharibyan220319@gmail.com>
2025-03-31 16:58:02 +0400

    commit2

---
+++ src/components/Card.js
@@ -0,0 +1,18 @@
+export const CardItem = () => {
+  return (
+    <div className="card-item">
+      <button>Button</button>
+    </div>
+  );
+};
+
+const Card = () => {
+  return (
+    <div className="card">
+      <h2>Card Title</h2>
+      <p>Some text</p>
+    </div>
+  );
+};
+
+export default Card;
